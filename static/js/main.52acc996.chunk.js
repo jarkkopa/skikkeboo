e48ea7c08f5e98768d3efc6199c0dbd4eb370030
Jarkko Palokangas <hpmandito@gmail.com>
2021-04-30 20:25:42 +0300

    Updates

---
+++ static/js/main.52acc996.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpskikkeboo=this.webpackJsonpskikkeboo||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),s=n.n(c),i=n(3),o=n.n(i),r=n(0),k=function(){return Object(r.jsx)("div",{children:Object(r.jsx)("header",{className:"text-2xl",children:"Skikkeb\xf6\xf6"})})};n(9);o.a.render(Object(r.jsx)(s.a.StrictMode,{children:Object(r.jsx)(k,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.52acc996.chunk.js.map
